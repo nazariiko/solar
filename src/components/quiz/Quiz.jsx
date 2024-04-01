16cfee2f4dddc41c4bedf7539384d3400c277ebe
nazariiko <nazarii.dev@gmail.com>
2024-04-02 00:25:48 +0300

    f

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -193,7 +193,7 @@ const Quiz = ({ handleHideHeader }) => {
           onClick={() => {
             setIsFirstScreen(false);
             handleHideHeader();
-            document.body.scrollTop = 0;
+            window.scrollTo({ top: 0, behavior: 'smooth' });
           }}
           className="first-screen-btn">
           Jetzt Preis-Check starten!
